85b13e4ef84b16afd47bd95ed9e19d09b81a8181
zdbrenda <zdbrenda@gmail.com>
2019-08-27 18:25:41 -0600

    Adding AJAX loading spinner and search results pagination

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import {elements} from './views/base';
+import {elements,renderLoader,clearLoader} from './views/base';
 //global state of the app
 // -search object
 
@@ -17,11 +17,12 @@ const controlSearch =async ()=>{
         //3. prepare UI
         searchView.clearInput();
         searchView.clearResults();
-        
+        renderLoader(elements.searchRes);
         //4. search the recipes
         await state.search.getResults();
         
         //5. render results on UI
+        clearLoader();
         searchView.renderResults(state.search.results);
         
     }
@@ -31,6 +32,15 @@ elements.searchForm.addEventListener('submit', e=>{
     e.preventDefault();
     controlSearch();
     
+});
+
+elements.searchResPages.addEventListener('click',e=>{
+    const btn=e.target.closest('.btn-inline');
+    if(btn){
+       const goToPage=parseInt(btn.dataset.goto,10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.results,goToPage);
+   }
 });
 
 
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -3,6 +3,7 @@ export const getInput=()=>elements.searchInput.value;
 export const clearInput= () => {elements.searchInput.value=""};
 export const clearResults = ()=>{
     elements.searchResList.innerHTML='';
+    elements.searchResPages.innerHTML='';
     
 };
 
@@ -38,8 +39,50 @@ export const renderRecipe=recipe=>{
                 </li>`;
     elements.searchResList.insertAdjacentHTML('beforeend',markup);
     
-}
-export const renderResults=recipes=>{
+};
+// type: 'prev' or 'next'
+const createButton=(page, type)=>`
+                <button class="btn-inline results__btn--${type}" data-goto=${type==='prev'?page-1:page+1}
+>
+                    <svg class="search__icon">
+                        <use href="img/icons.svg#icon-triangle-${type==='prev'?'left':'right'}"></use>
+                    </svg>
+                    <span>Page ${type==='prev'?page-1:page+1}</span>
+                </button>
+                
+
+`;
+
+const renderButtons=(page, numResults, resPerPage)=>{
+    const pages=Math.ceil(numResults/resPerPage);
+    let button;
+    if(page===1&&pages>1){
+       button= createButton(page,'next');
+        //only Button to go to next pages
+    }
+    else if(page===pages){
+        //only button to go to previous pages
+        button= createButton(page,'prev');
+    }
+    else{
+        //buttons to go to previous pages and next pages
+        
+        button= `${createButton(page,'prev')}
+                ${createButton(page,'next')}
     
-    recipes.forEach(renderRecipe);
+`;
+    }
+    
+    elements.searchResPages.insertAdjacentHTML('afterbegin',button);
+};
+
+
+export const renderResults=(recipes,page=1, resPerPage=10) =>{
+    //render results of current page
+    const start = (page-1)*resPerPage;
+    const end = (page)*resPerPage;
+    
+    recipes.slice(start,end).forEach(renderRecipe);
+    //render pagination buttons
+    renderButtons(page,recipes.length,resPerPage);
 }
